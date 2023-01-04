ecfc2950efdcea877c179635339610304023ce7e
yuedongli1 <2660417142@qq.com>
2023-01-04 14:36:37 +0800

    fixed:act

--- network/common.py
+++ network/common.py
@@ -82,7 +82,7 @@ class Conv(nn.Cell):
         if _SYNC_BN:
             self.bn = nn.SyncBatchNorm(c2, momentum=(1 - 0.03), eps=1e-3)
         self.bn = nn.BatchNorm2d(c2, momentum=(1 - 0.03), eps=1e-3)
-        self.act = nn.SiLU() if act is True else (act if isinstance(act, nn.Cell) else Identity())
+        self.act = act if isinstance(act, nn.Cell) else Identity()
 
     def construct(self, x):
         return self.act(self.bn(self.conv(x)))
